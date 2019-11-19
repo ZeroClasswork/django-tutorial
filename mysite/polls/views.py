ef8790f3aa32da5836fd5869889c47b8e5043c14
atleastzero <megan.obryan52@gmail.com>
2019-11-19 11:45:38 -0800

    Fixed typo (DetailView).

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,7 +25,7 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone)
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
